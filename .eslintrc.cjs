b06745606bc46907159104cb273ff163ff6ce235
AbtahiHasan <anikidpersonal@gmail.com>
2023-05-01 13:12:34 +0600

    banner left side done

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,7 @@ module.exports = {
     'no-unused-vars': 0,
     "no-undef": 0,
     'react/prop-types': 0,
+    "react/no-unescaped-entities": 0,
     'react-refresh/only-export-components': 'warn',
   },
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,9 +10,17 @@ const Home = () => {
                 <img className="h-screen w-full" src={bannerImg} alt="" />
             </div>
             <Container>
-                <Navbar />                
+                <Navbar />  
+                <section className="text-white absolute top-[70px] flex  h-[80vh]">
+                    <div className="flex justify-center flex-col md:w-[30%]">
+                        <h2 className="text-[70px] font-bold whitespace-nowrap">Cox's bazar</h2>
+                        <p>Cox's Bazar is a city, fishing port, tourism centre and district headquarters in southeastern Bangladesh. It is famous mostly for its long natural sandy beach, and it ...</p>
+                    </div>
+                    <div className="w-[70%]">
+
+                    </div>
+                </section>              
             </Container>
-            Home
         </div>
     );
 };
